241d63185494b695893e6e5879fc5e6fd9044988
João Miguel <joaomiguelmorais1108@gmail.com>
2024-10-23 20:52:50 -0300

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,11 @@
+senha = 0
+
+senha = int(input("Digite a senha: "))
+
+while (senha == 123):
+    
+    senha_c = int(input("Confirme a senha: "))
+    
+    print ("senha correta")
+
+print ("senha incorreta")
